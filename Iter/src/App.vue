17c44e31d6ddb581f841b68417e9ad38baf5a3ff
Life75 <austynwashington@yahoo.com>
2022-02-03 10:41:09 -0500

    Iter adding css elements and vue 3 comp

--- Iter/src/App.vue
+++ Iter/src/App.vue
@@ -1,21 +1,58 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <div>
+    <div id="nav">
+      <nav
+        class="
+          bg-white
+          border-gray-200
+          px-2
+          sm:px-4
+          py-2.5
+          dark:bg-gray-800
+        "
+      >
+        <div class="container flex flex-row ">
+          <a href="#" class="flex">
+            <span
+              class="
+                self-center
+                text-lg
+                font-semibold
+                whitespace-nowrap
+                dark:text-white
+              "
+              >Iter</span
+            >
+            <router-link
+              class="
+                text-slate-300
+                self-center
+                font-semibold
+                whitespace-nowrap
+                px-4
+      
+                py-2.5
+                dark:text-white
+              "
+              to="/"
+              >Anew Journey</router-link
+            >
+          </a>
+        </div>
+      </nav>
+      <router-link to="/About">About</router-link>
+      <router-view />
+    </div>
+  </div>
 </template>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
 }
 </style>
---
+++ Iter/src/views/Home.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="font-bold text-cyan-500">Welcome to Iter!</div>
+</template>
+
+<script>
+
+</script>
+
+
+<style>
+</style>
