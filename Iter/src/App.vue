aa3a82a07752e8f2eab79189e4790d25cd4656b2
awashington <awashington@healthqlix.com>
2022-02-28 12:00:49 -0500

    update to the iter nav bar

--- Iter/src/App.vue
+++ Iter/src/App.vue
@@ -7,18 +7,24 @@
   <div class>
     <div id="nav">
       <nav class="py-2.5 mix-blend-normal">
-        <div class="container flex">
-          <a href="#" class="self-center text-lg font-semibold whitespace-nowrap dark:text-white">
+        <div class="flex space-x-4 justify-center">
+          <a href="#" class=" self-center text-lg font-semibold whitespace-nowrap dark:text-white">
             <router-link
-              class="text-black text-left whitespace-nowrap mx-20 hover:underline font-sans text-2xl"
+              class="text-black text-left whitespace-nowrap hover:underline font-sans text-2xl"
               to="/"
             >Iter</router-link>
-            <span class="mr-10">
+            
+          </a>
+          <span class="flex justify-center  ">
               <router-link class="routerLink" to="/About">What is Iter?</router-link>
               <!-- Space this out more for login to the left -->
+              <router-link class="routerLink" to="#">Logs</router-link>
+
               <router-link class="routerLink" to="#">Login</router-link>
             </span>
-          </a>
+          <span class=" align-middle">
+            
+          </span>
         </div>
       </nav>
       <router-view />
@@ -28,7 +34,7 @@
 
 <style>
 .routerLink {
-  @apply   text-black text-center whitespace-nowrap px-4 py-2.5 hover:underline font-sans text-2xl;
+  @apply   text-black  text-center whitespace-nowrap px-4 py-2.5 hover:underline font-sans text-2xl;
 }
 
 body {
